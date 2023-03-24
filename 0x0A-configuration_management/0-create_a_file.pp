7aeb8881a321fe5d08e411ee0cf42279ec00e23c
Samuel Ibanga <ibangajnr27@gmail.com>
2023-03-24 23:08:50 +0000

    updated code

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Create a file in /tmp
 file { 'school':
-  path    => '/tmp/school'
-  content => 'I love Puppet'
-  group   => 'www-data'
-  owner   => 'www-data'
-  mode    => '0744'
+  path    => '/tmp/school',
+  content => 'I love Puppet',
+  group   => 'www-data',
+  owner   => 'www-data',
+  mode    => '0744',
 }
